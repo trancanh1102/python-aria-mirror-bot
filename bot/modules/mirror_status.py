fe9bc72d055a585bd933f4e720c56134c33a3b35
trancanh1102 <40576239+trancanh1102@users.noreply.github.com>
2020-10-26 19:26:52 +0700

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -12,7 +12,7 @@ import threading
 def mirror_status(update,context):
     message = get_readable_message()
     if len(message) == 0:
-        message = "No active downloads"
+        message = "Không có quá trình tải xuống nào đang hoạt động!"
         reply_message = sendMessage(message, context.bot, update)
         threading.Thread(target=auto_delete_message, args=(bot, update.message, reply_message)).start()
         return
